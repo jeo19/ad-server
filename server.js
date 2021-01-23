6b93d4708e9fb7bc5dbb72b66682875bf5ae4c95
jeo19 <terrakkk@outlook.com>
2021-01-23 10:22:02 +0800

    add:print the body content

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
-const http = require('http'); // import the server module
-http.createServer((request, response) => { // Creating the server method
-  console.log('server start!');
+const http = require('http');
+http.createServer((request, response) => {
+  return request
+    .on('error', (err) => { 
+      console.error(err);
+    })
+    .on('data', (data) => { 
+      console.log(data);
+    })
+    .on('end', () => { 
+      response.on('error', (err) => { 
+        console.error(err);
+      });
+      response.statusCode = 200; 
+       response.setHeader('Content-Type', 'text/plain'); 
+      response.write('hi\n'); 
+      response.end('the end!'); 
+    });
 }).listen(8080);
