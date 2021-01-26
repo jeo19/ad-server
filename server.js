9df0717e6b041ee138f2ae5e8101b4b20893e5a0
jeo19 <terrakkk@outlook.com>
2021-01-26 09:35:30 +0800

    chore:Install the express framework and run

--- server.js
+++ server.js
@@ -1,28 +1,8 @@
-const http = require('http');
-const url = require('url');
-const fs = require('fs');
-http.createServer((request, response) => {
-  const path = url.parse(request.url, true).pathname; 
-  if (request.method === 'GET') { 
-    if (path === '/about') { 
-      response.writeHead(200,{'Content-Type':'text/html'}); 
-      fs.readFile(__dirname + '/about.html', (err, data) => { 
-        if (err) {
-          return console.error(err); 
-        }
-        response.end(data, 'utf-8'); 
-      });
-    } else if (path === '/') { 
-      response.writeHead(200,{'Content-Type':'text/html'});
-      fs.readFile(__dirname + '/main.html', (err, data) => {
-        if (err) {
-          return console.error(err);
-        }
-        response.end(data, 'utf-8');
-      });
-    } else { 
-      response.statusCode = 404;
-      response.end('does not exit');
-    }
-  }
-}).listen(8080);
+const express=require('express');
+const app=express();
+app.get('/',(req,res)=>{
+  res.send('Hello world');
+});
+app.listen(8080,()=>{
+  console.log('Express App on port 8080');
+});
