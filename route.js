717cfedc5ebf5f7ae0778d95664dffe6be820f9f
jeo19 <terrakkk@outlook.com>
2021-01-28 09:48:32 +0800

    style:Use the pug template step2

--- route.js
+++ route.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const path = require('path');
-const router = express.Router(); // Separation a route
+const router = express.Router();
 router.get('/', (req, res) => {
-    // Connect to a route instead an app
-    res.sendFile(path.join(__dirname, 'html', 'main.html'));
+    res.render('main'); // (3)
 });
 router.get('/about', (req, res) => {
-    res.sendFile(path.join(__dirname, 'html', 'about.html'));
+    res.render('about'); // (4)
 });
-module.exports = router; // Make a moudle
+module.exports = router;
--- server.js
+++ server.js
@@ -2,14 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const route = require('./route.js');
+app.set('view engine', 'pug'); // (1)
+app.set('views', path.join(__dirname, 'html')); // (2)
+app.use(express.static(path.join(__dirname, 'html')));
 app.use('/', route);
-app.use((req, res, next) => {
-    res.status(404).send(`Doesn't correspondence address`);
-});
-app.use((err, req, res, next) => {
-    console.error(err.stack); //Display the errors
-    res.status(500).send('Error at a server');
-});
 app.listen(8080, () => {
     console.log('Express App on port 8080!');
 });
