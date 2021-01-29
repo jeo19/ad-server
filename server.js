e13d7a20897ee283263a4c60fbbbb39e1e1dbd8f
jeo19 <terrakkk@outlook.com>
2021-01-29 11:32:06 +0800

    add:Implement the connect of express with mongoose

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+const db = require('./db.js'); // import database
 const route = require('./route.js');
-app.set('view engine', 'pug'); // (1)
-app.set('views', path.join(__dirname, 'html')); // (2)
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'html'));
+db(); // The database running
 app.use(express.static(path.join(__dirname, 'html')));
 app.use('/', route);
+// Error handle
 app.listen(8080, () => {
     console.log('Express App on port 8080!');
 });
